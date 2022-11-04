90f2528a7b4b17a6029574ae5fe3bf52139a049a
sungjun111 <vlxl202@naver.com>
2022-11-04 09:05:13 +0900

    성준

--- src/main/java/com/ticket/biz/controller/LoginController.java
+++ src/main/java/com/ticket/biz/controller/LoginController.java
@@ -1,5 +1,9 @@
 package com.ticket.biz.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,43 +19,54 @@ import com.ticket.biz.pwtest.PwCheck;
 @Controller
 @SessionAttributes("login")
 public class LoginController {
-	//창일추가
+	// 창일추가
 	@Autowired
 	private PwCheck pwCheck;
-	//---
+	// ---
 	@Autowired
 	private MemberService memberService;
 
 	@RequestMapping(value = "/logincheck", method = RequestMethod.POST)
-	public String login(MemberVO vo, HttpSession session) {
-		//창일-추가
-		String password= vo.getMb_pw();
-		//---
-		
+	public String login(MemberVO vo, HttpSession session, HttpServletResponse response) {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		// 창일-추가
+		String password = vo.getMb_pw();
+		// ---
+
 		System.out.println("로그인 인증 처리...");
 		if (vo.getMb_id() == null || vo.getMb_id().equals("")) {
+			System.out.println("아이디 안적음");
 			throw new IllegalArgumentException("아이디는 반드시 입력해야 합니다.");
 		}
-		//창일 추가
-		boolean login= pwCheck.isMatch(vo.getMb_pw(),memberService.getMember(vo).getMb_pw());
-		 if(login==true) {
+		
+		if (memberService.getMember(vo) != null) {
+			boolean login = pwCheck.isMatch(vo.getMb_pw(), memberService.getMember(vo).getMb_pw());
+			if (login == true) {
 				System.out.println("로그인");
 				System.out.println(memberService.getMember(vo).getMb_id());
 				session.setAttribute("mb_Id", memberService.getMember(vo).getMb_id());
 				return "redirect:index.jsp";
-			}else {
+			} else {
 				System.out.println("실패");
 				return "redirect:login.jsp";
 			}
-		 //---
-//		if (memberService.loginCheck(vo) != null) {
-//			session.setAttribute("mb_Id", memberService.loginCheck(vo).getMb_id());
-//			
-//			System.out.println("아이디: " + memberService.loginCheck(vo).getMb_id());
-//			return "redirect:index.jsp";
-//		} else {
-//			return "redirect:login.jsp";
-//		}
+		} else {
+			response.setCharacterEncoding("utf-8");
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter script;
+			try {
+				script = response.getWriter();
+				script.println("<script>");
+				script.println("alert('존재하지 않는 아이디 입니다.');");
+				script.println("location.href = 'login.jsp'");
+				script.println("</script>");
+				script.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return "redirect:login.jsp";
 	}
 
 	@RequestMapping(value = "/logoutGO")
