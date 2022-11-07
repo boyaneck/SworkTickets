3db015693993f2b5fe5859f473611b6cda0d35d8
sungjun111 <vlxl202@naver.com>
2022-11-07 18:44:27 +0900

    성준 + 창일 + 원정 + 원선 + 세현

--- src/main/java/com/ticket/biz/good/GoodService.java
+++ src/main/java/com/ticket/biz/good/GoodService.java
@@ -7,21 +7,19 @@ import com.ticket.biz.exhibition.ExhibitionVO;
 public interface GoodService {
 	//CRUD 기능의 메소드 구현
 		//좋아요등록
-		void insertGood(GoodVO vo);
-
+		public int insertGood(GoodVO vo);
+		
 		//좋아요삭제
-		void deleteGood(GoodVO vo);
-
+		public int deleteGood(GoodVO vo);
+		
 		//좋아요순 조회
-		List<GoodVO> getGoodList();
+		public List<GoodVO> getGoodList();
 
-		//좋아요 체크
-		GoodVO getGoodChk(GoodVO vo);
+		public int getMyGoodListCnt(GoodVO vo);
 
-		//좋아요 체크
-		boolean getGoodYN(GoodVO vo);
+		public List<ExhibitionVO> getMyGoodList(GoodVO vo);
 
-		List<ExhibitionVO> getMyGoodList(GoodVO vo);
-
-		int getMyGoodListCnt(GoodVO vo);
+		
 }
+	
+
--- src/main/java/com/ticket/biz/good/GoodVO.java
+++ src/main/java/com/ticket/biz/good/GoodVO.java
@@ -9,57 +9,52 @@ public class GoodVO {
 	private int good_check;//체크
 	private String mb_id;//사용자아아디
 	private int exh_no;//전시아이디
-
-	private int offset;
-
-
-	@Autowired
-	private ExhibitionVO exhibitionvo;
-
-
-	public ExhibitionVO getExhibitionvo() {
-		return exhibitionvo;
-	}
-	public void setExhibitionvo(ExhibitionVO exhibitionvo) {
-		this.exhibitionvo = exhibitionvo;
+	
+	private int cnt;
+	
+	public int getCnt() {
+		return cnt;
 	}
 
-
-
-	public int getOffset() {
-		return offset;
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
 	}
-	public void setOffset(int offset) {
-		this.offset = offset;
-	}
+
 	public int getGood_id() {
 		return good_id;
 	}
+
 	public void setGood_id(int good_id) {
 		this.good_id = good_id;
 	}
-	public int getGood_check() {
-		return good_check;
-	}
-	public void setGood_check(int good_check) {
-		this.good_check = good_check;
-	}
+
 	public String getMb_id() {
 		return mb_id;
 	}
+
 	public void setMb_id(String mb_id) {
 		this.mb_id = mb_id;
 	}
+
 	public int getExh_no() {
 		return exh_no;
 	}
+
 	public void setExh_no(int exh_no) {
 		this.exh_no = exh_no;
 	}
 
 	@Override
 	public String toString() {
-		return "GoodVO [good_id=" + good_id + ", good_check=" + good_check + ", mb_id=" + mb_id + ", exh_no=" + exh_no
-				+ ", exhibitionvo=" + exhibitionvo + "]";
+		return "GoodVO [good_id=" + good_id + ", mb_id=" + mb_id + ", exh_no=" + exh_no + ", cnt=" + cnt + "]";
 	}
+
+	public void setOffset(int offset) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
 }
+	
