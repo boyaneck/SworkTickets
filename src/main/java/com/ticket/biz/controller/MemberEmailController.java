ba501fd59037031634fff208ab6a570f06f809d4
sungjun111 <vlxl202@naver.com>
2022-11-15 14:57:10 +0900

    아이디 / 비밀번호 찾기 기능

--- src/main/java/com/ticket/biz/controller/MemberEmailController.java
+++ src/main/java/com/ticket/biz/controller/MemberEmailController.java
@@ -13,60 +13,119 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ticket.biz.member.MemberService;
+import com.ticket.biz.member.MemberVO;
+
 @Controller
 public class MemberEmailController {
 
-	@Autowired
-	MailSender sender;
-	@Autowired
-	HttpSession session;
+   @Autowired
+   MailSender sender;
+   @Autowired
+   HttpSession session;
+   @Autowired
+   private MemberService memberService;
+   
+  
+   
+   // 인증번호 이메일 전송 Ajax
+   // 아이디 찾기
+   @ResponseBody
+   @RequestMapping(value = "/email_Send", method = RequestMethod.POST)
+   public String mail_Send(@RequestParam String email,MemberVO vo) {
+      vo.setMb_email(email);
+      if(memberService.find(vo)!=null) {
+      System.out.println("email_Send이동");
+      Random random = new Random();
+      String key = "";
+      SimpleMailMessage message = new SimpleMailMessage();
+      message.setTo(email);
 
-	// 인증번호 이메일 전송 Ajax
-	@ResponseBody
-	@RequestMapping(value = "/email_Send", method = RequestMethod.POST)
-	public String mail_Send(@RequestParam String email) {
-		System.out.println("email_Send이동");
-		Random random = new Random();
-		String key = "";
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setTo(email);
+      //보낼 인증번호 난수 생성 로직
+      for (int i = 0; i < 3; i++) {
+         // A~Z(대문자)까지 랜덤 알파벳 생성
+         int index = random.nextInt(25) + 65;
+         key += (char) index;
+         // 랜덤 정수를 생성
+         int numIndex = random.nextInt(10);
+         key += numIndex;
+      }
+      //이메일의 제목이 되는 부분
+      message.setSubject("인증번호 입력을 위한 메일 전송");
+      //이메일의 내용이 되는 부분
+      message.setText("인증 번호 : " + key);
+      //이메일의 보내는 사람이 되는 부분(반드시 smtp설정한 이메일주소 입력, 다를 경우 인증 안됨) 예시: admin@gmail.com 등..
+      message.setFrom("hm_tickets@naver.com");
+      System.out.println("인증번호 값: "+key);
 
-		//보낼 인증번호 난수 생성 로직
-		for (int i = 0; i < 3; i++) {
-			// A~Z(대문자)까지 랜덤 알파벳 생성
-			int index = random.nextInt(25) + 65;
-			key += (char) index;
-			// 랜덤 정수를 생성
-			int numIndex = random.nextInt(10);
-			key += numIndex;
-		}
-		//이메일의 제목이 되는 부분
-		message.setSubject("인증번호 입력을 위한 메일 전송");
-		//이메일의 내용이 되는 부분
-		message.setText("인증 번호 : " + key);
-		//이메일의 보내는 사람이 되는 부분(반드시 smtp설정한 이메일주소 입력, 다를 경우 인증 안됨) 예시: admin@gmail.com 등..
-		message.setFrom("hm_tickets@naver.com");
-		System.out.println("인증번호 값: "+key);
+      sender.send(message);
+      System.out.println("메세지 : " + message);
+      session.setAttribute("emailKey", key);
+      System.out.println("key :" + key);
+      return "ok";
+            }
+      else {
+         return "error";
+      }
+   }
+   // 비밀번호 찾기
+   @ResponseBody
+   @RequestMapping(value = "/email_Send1", method = RequestMethod.POST)
+   public String mail_Send1(@RequestParam String email, @RequestParam String mb_id, MemberVO vo) {
+      vo.setMb_email(email);
+//      vo.setMb_id(mb_id);
+      System.out.println(mb_id);
+      if(mb_id!=null) {
+    	  vo.setMb_id(mb_id);
+      }
+      System.out.println(vo.toString());
+      if(memberService.find(vo)!=null) {
+      System.out.println("email_Send이동");
+      Random random = new Random();
+      String key = "";
+      SimpleMailMessage message = new SimpleMailMessage();
+      message.setTo(email);
 
-		sender.send(message);
-		System.out.println("메세지 : " + message);
-		session.setAttribute("emailKey", key);
-		System.out.println("key :" + key);
-		return "ok";
-	}
+      //보낼 인증번호 난수 생성 로직
+      for (int i = 0; i < 3; i++) {
+         // A~Z(대문자)까지 랜덤 알파벳 생성
+         int index = random.nextInt(25) + 65;
+         key += (char) index;
+         // 랜덤 정수를 생성
+         int numIndex = random.nextInt(10);
+         key += numIndex;
+      }
+      //이메일의 제목이 되는 부분
+      message.setSubject("인증번호 입력을 위한 메일 전송");
+      //이메일의 내용이 되는 부분
+      message.setText("인증 번호 : " + key);
+      //이메일의 보내는 사람이 되는 부분(반드시 smtp설정한 이메일주소 입력, 다를 경우 인증 안됨) 예시: admin@gmail.com 등..
+      message.setFrom("hm_tickets@naver.com");
+      System.out.println("인증번호 값: "+key);
 
-	// 이메일 인증번호 체크 Ajax
-	@ResponseBody
-	@RequestMapping(value = "/email_Check", method = RequestMethod.POST)
-	public boolean mail_Check(String emailCheck) {
-		boolean result = false;
-		String emailKey = (String) session.getAttribute("emailKey");
-		System.out.println("보낸 인증번호 값 : " + emailKey + ", 사용자가 입력한 값 : " + emailCheck);
-		if (emailCheck.equals(emailKey)) {
-			result = true;
-		}
-		/* session.removeAttribute("emailKey"); */
-		return result;
-	}
+      sender.send(message);
+      System.out.println("메세지 : " + message);
+      session.setAttribute("emailKey", key);
+      System.out.println("key :" + key);
+      return "ok";
+            }
+      else {
+         return "error";
+      }
+   }
+
+   // 이메일 인증번호 체크 Ajax
+   @ResponseBody
+   @RequestMapping(value = "/email_Check", method = RequestMethod.POST)
+   public boolean mail_Check(String emailCheck) {
+      boolean result = false;
+      String emailKey = (String) session.getAttribute("emailKey");
+      System.out.println("보낸 인증번호 값 : " + emailKey + ", 사용자가 입력한 값 : " + emailCheck);
+      if (emailCheck.equals(emailKey)) {
+         result = true;
+      }
+      /* session.removeAttribute("emailKey"); */
+      return result;
+   }
 
 }
